688d78bf14b7aeebc9bc25ef1dbcd20ad978ec26
hasharas <hasharamsankalpam@gmail.com>
2024-12-20 02:37:11 +0530

    ADD:function add display to 10 product

--- src/components/LatestCollection.jsx
+++ src/components/LatestCollection.jsx
@@ -1,13 +1,30 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { ShopContext } from '../context/ShopContext';
+import Title from './Title';
 
 const LatestCollection = () => {
 
+
+
       const { products } = useContext(ShopContext);
-      console.log(products);
+      // console.log(products);
+      const [latestProduct, setLatestProduct] = useState([]);
+
+      useEffect(() => {
+            setLatestProduct(products.slice(0, 10));
+      }, [])
+
 
       return (
-            <div>
+            <div className='m-10 '>
+                  <div className='py-8 text-center text-3xl'>
+                        <Title text1={"LATEST"} text2={"COLLECTION"} />
+                        <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
+                              Find a beautiful coffee color palette from Color Hunt's curated collection
+                        </p>
+
+                  </div>
+
 
             </div>
       );
