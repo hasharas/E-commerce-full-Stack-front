826c3d220a7fb86491544ebaf2a73ac3e0d40a3e
hasharas <hasharamsankalpam@gmail.com>
2024-12-20 11:59:35 +0530

    Finish:latest collection style complete

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -7,7 +7,7 @@ const ProductItem = ({ id, image, name, price }) => {
       const { currency } = useContext(ShopContext);
 
       return (
-            <Link to={`/product/${id}`} className='text-gray-700 cursor-pointer'>
+            <Link to={`/product/${id}`} className='text-gray-700 border cursor-pointer'>
                   <div className='overflow-hidden'>
                         <img src={image[0]} className='hover:scale-110 transition ease-in-out' alt="cart image" />
                   </div>
