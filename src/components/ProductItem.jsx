457b65db41d967a5a13013ab268e060d85c3de49
hasharas <hasharamsankalpam@gmail.com>
2024-12-20 11:07:54 +0530

    complete:product item component

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -7,7 +7,12 @@ const ProductItem = ({ id, image, name, price }) => {
       const { currency } = useContext(ShopContext);
 
       return (
-            <Link >
+            <Link to={`/product/${id}`} className='text-gray-700 cursor-pointer'>
+                  <div className='overflow-hidden'>
+                        <img src={image[0]} className='hover:scale-110 transition ease-in-out' alt="cart image" />
+                  </div>
+                  <p className='pt-3 pb-1 text-sm'>{name}</p>
+                  <p className='text-sm font-medium'>{currency}{price}</p>
 
             </Link>
       );
