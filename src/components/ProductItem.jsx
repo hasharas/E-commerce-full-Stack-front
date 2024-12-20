7b71c284b73b72b59ecebe877f597d0229890af4
hasharas <hasharamsankalpam@gmail.com>
2024-12-20 10:57:13 +0530

    Create:product item file

---
+++ src/components/ProductItem.jsx
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react';
+import { ShopContext } from '../context/ShopContext';
+import { Link } from 'react-router-dom';
+
+const ProductItem = ({ id, image, name, price }) => {
+
+      const { currency } = useContext(ShopContext);
+
+      return (
+            <Link >
+
+            </Link>
+      );
+}
+
+export default ProductItem;
