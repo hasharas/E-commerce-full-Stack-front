89d7254b098407ac7a6b2f05432d0bb48628f674
hasharas <hasharamsankalpam@gmail.com>
2024-12-21 14:42:00 +0530

    create:bestseller section create

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,8 +7,8 @@ const Navbar = () => {
 
       return (
             <div className='flex items-center justify-between  py-5 font-medium'>
+                  <Link to='/'><img src={assets.zippo_logo} alt="logo" className='w-36' /></Link>
 
-                  <img src={assets.zippo_logo} alt="logo" className='w-36' />
                   <ul className='hidden sm:flex gap-5 text-sm font-semibold' >
                         <NavLink to='/' className='flex flex-col items-center gap-1'>
                               <p>HOME</p>
--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -7,7 +7,7 @@ const ProductItem = ({ id, image, name, price }) => {
       const { currency } = useContext(ShopContext);
 
       return (
-            <Link to={`/product/${id}`} className='text-gray-700 border cursor-pointer'>
+            <Link to={`/product/${id}`} className='text-gray-700 border cursor-pointer hover:shadow-lg transition'>
                   <div className='overflow-hidden'>
                         <img src={image[0]} className='hover:scale-110 transition ease-in-out' alt="cart image" />
                   </div>
