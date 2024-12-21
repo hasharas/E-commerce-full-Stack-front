56d102406aff4ede07567c3e802881773b943470
hasharas <hasharamsankalpam@gmail.com>
2024-12-21 23:44:21 +0530

    start:create policy component

---
+++ src/components/OurPolicy.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { assets } from '../assets/frontend_assets/assets';
+
+const OurPolicy = () => {
+      return (
+            <div>
+                  <div>
+                        <img src={assets.exchange_icon} alt="" />
+                  </div>
+
+            </div>
+      );
+}
+
+export default OurPolicy;
