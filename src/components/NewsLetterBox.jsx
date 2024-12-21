b38f8ceef4f93356a34e25cbda57c6292390782f
hasharas <hasharamsankalpam@gmail.com>
2024-12-22 00:30:44 +0530

    Start: create news letter box

---
+++ src/components/NewsLetterBox.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const NewsLetterBox = () => {
+      return (
+            <div className=''>
+                  <p className='text-gray-700'>Subscribe now & get 20% off</p>
+                  <p className=''>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                  <div className='border border-gray-700'>
+                        <input type="text" className='' />
+                  </div>
+            </div>
+      );
+}
+
+export default NewsLetterBox;
