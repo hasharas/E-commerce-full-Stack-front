852df01e3f0253dbdcf269a29594999781a77e3d
hasharas <hasharamsankalpam@gmail.com>
2024-12-21 23:51:06 +0530

    Add:style adding policy component

--- src/components/OurPolicy.jsx
+++ src/components/OurPolicy.jsx
@@ -3,9 +3,11 @@ import { assets } from '../assets/frontend_assets/assets';
 
 const OurPolicy = () => {
       return (
-            <div>
+            <div className='flex flex-col sm:flex-row justify-around gap-12 sm:gap-2 text-center py-20 text-xs sm:text-sm md:text-base text-gray-700'>
                   <div>
-                        <img src={assets.exchange_icon} alt="" />
+                        <img src={assets.exchange_icon} className='w-12 m-auto mb-5' alt="exchnage icon" />
+                        <p className='font-semibold '>Easy Exchange Policy</p>
+                        <p className='text-gray-400'>We offer hassly free exchange policy</p>
                   </div>
 
             </div>
