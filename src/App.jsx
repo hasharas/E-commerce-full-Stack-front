493f78a2001e2ada9e42cdc9d53db9a36883be05
hasharas <hasharamsankalpam@gmail.com>
2024-12-22 00:46:56 +0530

    STyle:complete news letter box componett

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import Navbar from './components/Navbar';
 
 const App = () => {
   return (
-    <div className='px-4 bg-[#ffffff] sam:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+    <div className='px-4 bg-[#ffffff] sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
--- src/components/NewsLetterBox.jsx
+++ src/components/NewsLetterBox.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 
 const NewsLetterBox = () => {
       return (
-            <div className=''>
-                  <p className='text-gray-700'>Subscribe now & get 20% off</p>
-                  <p className=''>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-                  <div className='border border-gray-700'>
-                        <input type="text" className='' />
-                  </div>
+            <div className='flex flex-col items-center'>
+                  <p className='text-gray-800 text-2xl font-medium'>Subscribe now & get 20% off</p>
+                  <p className='text-gray-400'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                  <form className='w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3'>
+                        <input type="email" className='w-full sm:flex-1 outline-none' placeholder='Enter Your Email' required />
+                        <button type='submit' className='bg-black text-white text-xs px-10 py-4'>SUBSCRIBE</button>
+                  </form>
             </div>
       );
 }
