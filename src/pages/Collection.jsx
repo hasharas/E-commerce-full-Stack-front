404d2beca2258768c9e369596d90cc176562d0ba
hasharas <hasharamsankalpam@gmail.com>
2024-12-29 23:52:45 +0530

    Add:serch quary add

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -36,6 +36,10 @@ const Collection = () => {
 
             let productsCopy = products.slice();
 
+            if (showSearch && search) {
+                  productsCopy = productsCopy.filter(item => item.name.toLowerCase().includes(search.toLowerCase()));
+            }
+
             if (category.length > 0) {
                   productsCopy = productsCopy.filter(item => category.includes(item.category));
             }
@@ -72,7 +76,7 @@ const Collection = () => {
 
       useEffect(() => {
             applyFilter();
-      }, [category, subCategory])
+      }, [category, subCategory, search, showSearch])
 
       useEffect(() => {
             sortProduct();
