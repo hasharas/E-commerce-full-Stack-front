9d4340178ad9b19faec88f5e22ec4b877e45d8cf
hasharas <hasharamsankalpam@gmail.com>
2025-01-01 22:04:06 +0530

    image arry view product page

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -7,6 +7,8 @@ const Product = () => {
       const { productId } = useParams();
       const { products } = useContext(ShopContext);
       const [productData, setProductData] = useState(false);
+      const [image, setImage] = useState('');
+
 
 
       const fetchProductData = async () => {
@@ -14,7 +16,7 @@ const Product = () => {
             products.map((item) => {
                   if (item._id === productId) {
                         setProductData(item);
-                        console.log(item);
+                        setImage(item.image[0])
                         return null;
                   }
             })
@@ -24,11 +26,21 @@ const Product = () => {
             fetchProductData();
       }, []);
 
-      return (
-            <div>
+      return productData ? (
+            <div className='border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100'>
+                  {/* product data */}
+                  <div className='flex gap-12 sm:gap-12 flex-col sm:flex-row'>
+                        {/* product image */}
+                        <div className='flex-1 flex flex-col-reverse gap-3 sm:flex-row'>
+                              <div className='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full'>
+
+                              </div>
+
+                        </div>
+                  </div>
 
             </div>
-      );
+      ) : <div className='opacity-0'></div>
 }
 
 export default Product;
