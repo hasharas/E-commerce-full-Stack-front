625cbe883cdda6f2ea9f78745ad6148e99935216
hasharas <hasharamsankalpam@gmail.com>
2025-01-21 01:59:50 +0530

    create:product copmponent and add function

--- src/components/RelatedProduct.jsx
+++ src/components/RelatedProduct.jsx
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { ShopContext } from '../context/ShopContext';
 
 const RelatedProduct = ({ category, subCategory }) => {
+
+
+      const { product } = useContext(ShopContext);
+      const [related, setRelated] = useState([]);
+
+      useEffect(() => {
+            if (product.lenth > 0) {
+                  let productCopy = product.slice();
+
+                  productCopy = productCopy.filter((item) => category === item.category);
+                  productCopy = productCopy.filter((item) => subCategory === item.subCategory);
+
+                  console.log(productCopy.slice(0, 5));
+            }
+      }, [product])
+
       return (
             <div>
 
